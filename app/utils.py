97c41049ca9b1cdf30e7e6e7ecadac77dbbc9015
Prithvi Raj Munday <prmunday@Prithvis-MacBook-Pro.local>
2022-08-10 23:51:09 +1000

    Testing PTM maker in output excel sheet v1

--- app/utils.py
+++ app/utils.py
@@ -308,6 +308,9 @@ def saveBindersData(taskId, alleles, method):
                 # Original upload file used to derive all other columns present in the input file
                 input_file = pd.read_csv('{}/{}/{}/{}.csv'.format(data_mount,taskId,sample,replicate[:-13]))
 
+                # Adding Colunm to represen the peptides without the PTM changes
+                input_file['PlainPeptide'] = input_file.apply(lambda x : omitPTMContent(x['Peptide']),axis=1)
+
                 # Initialsing the allele and binders collection
                 alleles_dict = {}
                 
@@ -371,8 +374,11 @@ def saveBindersData(taskId, alleles, method):
                         # Sorting the binders from stong to weak binding level and saving it.
                         alleles_dict[allele] = alleles_dict[allele].sort_values(by=['Score'],ascending=False)[["Peptide","Score","Binding Level","Control"]]
 
+                        # Updating the name of binding results column Peptide to PlainPeptide
+                        alleles_dict[allele].rename(columns={'Peptide': 'PlainPeptide'}, inplace=True)
+
                         # Adding the meta-data from the original input file
-                        alleles_dict[allele] = alleles_dict[allele].merge(input_file, on='Peptide',how='left')
+                        alleles_dict[allele] = alleles_dict[allele].merge(input_file, on='PlainPeptide',how='left')
 
                         alleles_dict[allele].to_csv('app/static/images/{}/{}/{}/{}/binders/{}/{}_{}_{}_binders.csv'.format(taskId,sample,method,replicate[:-13],allele_fromatted,replicate[:-13],allele_fromatted,method), index=False)
 
