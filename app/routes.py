38c075a2374279795981a2c63a49414803fbb3e9
Prithvi Raj Munday <prmunday@Prithvis-MacBook-Pro.local>
2022-08-14 13:39:47 +1000

    Demo page update

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,7 @@ project_root = os.path.dirname(os.path.realpath(os.path.join(__file__, "..")))
 TASK_COUNTER = 0
 
 # DEMO Task ID
-DEMO_TASK_ID = "202206212250395"
+DEMO_TASK_ID = "202208141201189"
 
 data_mount = app.config['IMMUNOLYSER_DATA']
 
