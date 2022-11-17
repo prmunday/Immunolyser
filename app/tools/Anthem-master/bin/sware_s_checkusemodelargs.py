8093af39e2aedfc492a6a42f356a09c5b3ec5caa
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 20:19:46 +0000

    Adding tarfile member sanitization to extractall()

--- app/tools/Anthem-master/bin/sware_s_checkusemodelargs.py
+++ app/tools/Anthem-master/bin/sware_s_checkusemodelargs.py
@@ -29,7 +29,29 @@ def chaeckusemodelargs(args):
 
     ###get length of matrix
     with tarfile.open(args.modelfile, 'r') as tar:
-        tar.extractall('model')
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, "model")
     with open('model/allmatrix.json', 'r') as f:
         allmatrix = json.load(f)
         
