a724a9280d75a03e070e01b5ae140a22a8f60726
prmunday <prmunday@gmail.com>
2024-06-14 12:24:33 +1000

    setting GibbsCluster setting use trash cluster to remove outliers (switch) to yes everytime

--- app/gibbscluster.py
+++ app/gibbscluster.py
@@ -16,6 +16,7 @@ for sample in os.listdir('{}/{}'.format(data_mount,task_id)):
                     -G ./../../../../../../../../app/tools/seq2logo-2.1/Seq2Logo.py \
                     -g 1-5 \
                     -k {} \
+                    -T \
                     -R {}/app/static/images/{}/{}/gibbscluster/{} '.format(data_mount,task_id, sample, replicate, os.cpu_count(), os.getcwd(), task_id, sample, replicate[:-9]))
 
             print(os.popen('perl ./app/tools/gibbscluster-2.0/GibbsCluster-2.0e_SA.pl \
@@ -24,4 +25,5 @@ for sample in os.listdir('{}/{}'.format(data_mount,task_id)):
                     -G ./../../../../../../../../app/tools/seq2logo-2.1/Seq2Logo.py \
                     -g 1-5 \
                     -k {} \
+                    -T \
                     -R {}/app/static/images/{}/{}/gibbscluster/{} '.format(data_mount,task_id, sample, replicate, os.cpu_count(), os.getcwd(), task_id, sample, replicate[:-9])).read())
