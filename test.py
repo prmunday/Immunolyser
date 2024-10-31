03dc255d6d60d48cad1ffaba7dca247c04c69ac1
prmunday <prmunday@gmail.com>
2024-10-31 17:02:41 +1100

    File to unit test methods

---
+++ test.py
@@ -0,0 +1,10 @@
+from app import utils
+from constants import *
+import app
+
+if __name__ == "__main__":
+    # Replace this with relevant test cases for your method
+    result = utils.generateBindingPredictions("6fa3cf66-72a7-4e9d-9165-a4a46cfd38c0", "HLA-A0201", Class_One_Predictors.MHCflurry)
+    print(result)
+
+    
