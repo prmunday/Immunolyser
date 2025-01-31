c434925cecd8545c0e79a8e39fd1abab0672ce7e
Prithvi Raj Munday <prmunday@gmail.com>
2025-01-31 20:57:39 +0800

    updated gibbs cluster script to use project root to determine seq2logo path

--- app/gibbscluster.py
+++ app/gibbscluster.py
@@ -1,42 +1,42 @@
 import sys
 import os
 from subprocess import call
-# from constants import *
 
+# Get command-line arguments
 task_id = sys.argv[1]
 data_mount = sys.argv[2]
 mhc_class = sys.argv[3]
 motif_length = int(sys.argv[4])
 
-# Set -C (perform Single sequence moves at every iteration (default is every -r iterations)) based on mhc class
+# Determine the project root
+project_root = os.path.dirname(os.path.realpath(os.path.join(__file__, "..")))
+
+# Set -C argument based on MHC class
 arg_C = '-C' if mhc_class == "One" else ''
 
 # Argument to specify the motif length
 arg_l = f'-l {motif_length}'
 
-for sample in os.listdir('{}/{}'.format(data_mount,task_id)):
-    for replicate in os.listdir('{}/{}/{}'.format(data_mount,task_id,sample)):
+# Set seq2logo path
+seq2logo_path = os.path.join(project_root, "app/tools/seq2logo-2.1/Seq2Logo.py")
 
-        if replicate[-9:] == f'_{motif_length}mer.txt':
-            print('Replicate file: ', replicate)
-            print("Gibbs Command: ", 'perl ./app/tools/gibbscluster-2.0/GibbsCluster-2.0e_SA.pl \
-                    -f {}/{}/{}/{} \
-                    -H R \
-                    -G ./../../../../../../../../app/tools/seq2logo-2.1/Seq2Logo.py \
-                    -g 1-5 \
-                    -k {} \
-                    -T \
-                    {} \
-                    {} \
-                    -R {}/app/static/images/{}/{}/gibbscluster/{} '.format(data_mount,task_id, sample, replicate, os.cpu_count(), arg_C, arg_l, os.getcwd(), task_id, sample, replicate[:-9]))
-
-            print(os.popen('perl ./app/tools/gibbscluster-2.0/GibbsCluster-2.0e_SA.pl \
-                    -f {}/{}/{}/{} \
-                    -H R \
-                    -G ./../../../../../../../../app/tools/seq2logo-2.1/Seq2Logo.py \
-                    -g 1-5 \
-                    -k {} \
-                    -T \
-                    {} \
-                    {} \
-                    -R {}/app/static/images/{}/{}/gibbscluster/{} '.format(data_mount,task_id, sample, replicate, os.cpu_count(), arg_C, arg_l, os.getcwd(), task_id, sample, replicate[:-9])).read())
+task_path = os.path.join(data_mount, task_id)
+for sample in os.listdir(task_path):
+    sample_path = os.path.join(task_path, sample)
+    
+    for replicate in os.listdir(sample_path):
+        if replicate.endswith(f'_{motif_length}mer.txt'):
+            print('Replicate file:', replicate)
+            gibbs_command = (
+                f'perl {project_root}/app/tools/gibbscluster-2.0/GibbsCluster-2.0e_SA.pl '
+                f'-f {sample_path}/{replicate} '
+                f'-H R '
+                f'-G {seq2logo_path} '
+                f'-g 1-5 '
+                f'-k {os.cpu_count()} '
+                f'-T {arg_C} {arg_l} '
+                f'-R {project_root}/app/static/images/{task_id}/{sample}/gibbscluster/{replicate[:-9]}'
+            )
+            
+            print("Gibbs Command:", gibbs_command)
+            print(os.popen(gibbs_command).read())
